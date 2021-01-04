fbd11d287add2c8a14a3cb4f29fda2660b792381
12wollmana <awollman2@gmail.com>
2021-01-04 13:59:26 -0600

    Implementing the Users Routes

--- app.js
+++ app.js
@@ -154,6 +154,57 @@ app
   .delete(deleteTour);
 
 /**
+ * User Handlers
+ */
+
+const getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined.',
+  });
+};
+
+const createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined.',
+  });
+};
+
+const getUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined.',
+  });
+};
+
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined.',
+  });
+};
+
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined.',
+  });
+};
+
+/**
+ * Users Routes
+ */
+
+app.route('/api/v1/users').get(getAllUsers).post(createUser);
+
+app
+  .route('/api/v1/users/:id')
+  .get(getUser)
+  .patch(updateUser)
+  .delete(deleteUser);
+
+/**
  * Start Server
  */
 const port = 3000;

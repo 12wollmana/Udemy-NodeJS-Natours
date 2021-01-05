7d8106316ed0e6e7a7703c454b1fad91a99e7bcd
12wollmana <awollman2@gmail.com>
2021-01-05 14:08:33 -0600

    Fix ESLint Errors

--- server.js
+++ server.js
@@ -9,5 +9,6 @@ const app = require('./app');
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
+  // eslint-disable-next-line no-console
   console.log(`App running on port ${port}...`);
 });

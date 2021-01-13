ea7b630839043392d4e071749a3e765422b5ef4e
12wollmana <awollman2@gmail.com>
2021-01-13 14:18:29 -0600

    Refactoring for MVC

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,24 +1,7 @@
 /**
  * Tour Handlers
  */
-const fs = require('fs');
-
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
-
-exports.checkID = (req, res, next, val) => {
-  const id = req.params.id * 1; // convert to number
-  if (id > tours.length) {
-    return res
-      .status(404) // 404 - Not Found
-      .json({
-        status: 'fail',
-        message: 'Invalid ID',
-      });
-  }
-  next();
-};
+const Tour = require('../models/tourModel');
 
 exports.checkBody = (req, res, next) => {
   const { body } = req;
@@ -34,54 +17,35 @@ exports.checkBody = (req, res, next) => {
 };
 
 exports.getAllTours = (req, res) => {
-  //console.log(req.requestTime);
-
   res.status(200).json({
     status: 'success', // can be success, failed, error,
     requestedAt: req.requestTime,
-    results: tours.length, // use when sending an array
-    data: {
-      tours, // same as tours: tours
-    },
+    // results: tours.length, // use when sending an array
+    // data: {
+    //   tours, // same as tours: tours
+    // },
   });
 };
 
 exports.getTour = (req, res) => {
-  //console.log(req.params);
-
   const id = req.params.id * 1; // convert to number
-  const tour = tours.find((el) => el.id === id);
+  // const tour = tours.find((el) => el.id === id);
 
-  res.status(200).json({
-    status: 'success',
-    data: { tour },
-  });
+  // res.status(200).json({
+  //   status: 'success',
+  //   data: { tour },
+  // });
 };
 
 exports.createTour = (req, res) => {
-  //   console.log(req.body);
-
-  const newId = tours[tours.length - 1].id + 1;
-  // eslint-disable-next-line prefer-object-spread
-  const newTour = Object.assign({ id: newId }, req.body); // Merge two objects
-
-  tours.push(newTour);
-
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    // eslint-disable-next-line no-unused-vars
-    (_err) => {
-      res
-        .status(201) // 201 - Created
-        .json({
-          status: 'success',
-          data: {
-            tour: newTour,
-          },
-        });
-    }
-  );
+  // res
+  //       .status(201) // 201 - Created
+  //       .json({
+  //         status: 'success',
+  //         data: {
+  //           tour: newTour,
+  //         },
+  //       });
 };
 
 exports.updateTour = (req, res) => {
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -9,8 +9,6 @@ const tourController = require('../controllers/tourController');
 
 const router = express.Router();
 
-router.param('id', tourController.checkID);
-
 router
   .route('/')
   .get(tourController.getAllTours)
